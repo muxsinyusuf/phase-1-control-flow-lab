4d4b0522c1352d3c8628b6752c6bb596bdc3b415
Muxsinyusuf <mucsin.yusuf@student.moringaschool.com>
2023-07-10 22:14:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let cost;
+  
+  if (feet <= 400) {
+    cost = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    cost = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    cost = 'No can do.';
+  } else {
+    cost = 0.02 * (feet - 400);
+  }
+  
+  return cost;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let message;
+  
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+  }
+  
+  return message;
 }
